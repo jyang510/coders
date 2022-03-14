0da96e42240e1be2e9bd345fe87d0e16418347b0
jyang <60086935+jyang510@users.noreply.github.com>
2022-03-14 23:58:20 +0900

    문자열 내 p와 y의 개수
    1. 문자열을 소문자로 바꾸고
    2. p와 y문자의 개수에 따라 count 값을 조정한 후에, 조정된 count값이 0인지 여부에 따라 boolean값을 리턴합니다

---
+++ 0314.js
@@ -0,0 +1,11 @@
+https://programmers.co.kr/learn/courses/30/lessons/12916
+
+function solution(s){
+    let str= s.toLowerCase();
+    let count = 0;
+    for(let i = 0; i < s.length; i++){
+        if(str[i] === 'p') count++;
+        else if(str[i] === 'y') count--;
+    }
+    return count === 0 ? true : false;
+}
