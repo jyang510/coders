78bf371bc1df9e548d54d59580c7e346fbe5462d
jyang <60086935+jyang510@users.noreply.github.com>
2022-03-27 00:04:26 +0900

    가운데 글자 가져오기
    1. s가 짝수인 경우엔 중간 앞글자도 추가해준다
    2. parseInt(s.length / 2)를 통해 s의 중간값을 구하여 answer에 더하고 리턴한다

--- 0325.js
+++ 0325.js
@@ -1,9 +1,13 @@
 https://programmers.co.kr/learn/courses/30/lessons/12925
 
+//이렇게 양자택일의 문제인 경우엔 리턴 값에 단순히 삼항연산자를 통해 나눠도 될 것 같다
+// 코드가 줄어드는 건 좋은 거라서,이렇게 쓸데없이 길 이유는 없는 것 같다
 function solution(s) {
-    let n = 1;
-    if (s[0] === '-') {
-        return - Number(s.substr(1, s.length - 1));
+    let answer = '';
+    if (s.length % 2 === 0) {
+        answer += s[s.length / 2 - 1];
     }
-    return Number(s);
+    answer += s[parseInt(s.length / 2)];
+    
+    return answer;
 }
