a5f7397ca8b50696fbf124f8b0995550bc696dfb
jyang <60086935+jyang510@users.noreply.github.com>
2022-03-30 23:59:46 +0900

    3/30 다른 사람 풀이 추가

--- 0322.js
+++ 0322.js
@@ -14,3 +14,24 @@ function solution(s){
     }
     return (arr.length > 0) ? 0 : 1;
 }
+
+// 3/30 다른 사람 풀이 추가
+// 이 문제 말고도 몇 문제 추가로 작성할 예정
+
+// 이번엔 다른 분들이 푼 방식과 내가 푼 방식이 다른 점이 거의 없어서 기분이 좋았다. 슬슬 나도 프로그래머스에 맞는 코드를 짜고 있나?
+// 방식 자체가 유사해 특별히 더 뭘 붙이지 않고 코드만 작성한다 
+
+const solution = (s) => {
+  if (s.length % 2 != 0) return 0;
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const b = s.charAt(i);
+    if (stack[stack.length - 1] === b) {
+      stack.pop();
+    } else {
+      stack.push(b);
+    }
+  }
+
+  return stack.length > 0 ? 0 : 1;
+};
